82e064478c3a91317bcf620d630f616080952f7e
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-05-25 11:38:38 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Alien Dictionary/alien-dictionary.cpp
@@ -0,0 +1,104 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    private:
+    void dfs(vector<int> adj[] , int node , int vis[] , stack<int> &st) {
+        vis[node] = 1;
+        
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(adj , it , vis , st);
+            }
+        }
+        st.push(node);
+    }
+    public:
+    string findOrder(string dict[], int N, int K) {
+        // converting given dictionary in adj list
+        vector<int> adj[K];
+        string ans = "";
+        for(int i = 0 ; i < N-1 ; i++) {
+            string s1 = dict[i];
+            string s2 = dict[i+1];
+            
+            int strLen = min(s1.size() , s2.size());
+            
+            for(int ptr = 0 ; ptr < strLen ; ptr++) {
+                if(s1[ptr] != s2[ptr]) {
+                adj[s1[ptr] - 'a'].push_back(s2[ptr]-'a');
+                break;
+                }
+            }
+        }
+        // For calling dfs
+        int vis[K] = {0};
+        stack<int> st;
+        
+        for(int i = 0 ; i < K ; i++) {
+            if(!vis[i]) {
+                dfs(adj , i , vis , st);
+            }
+        }
+        // Converting for sendign answer
+        while (!st.empty()) {
+			ans.push_back(st.top() + 'a');
+			st.pop();
+		}
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+string order;
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++) cin >> dict[i];
+        
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++) order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i]) f = false;
+
+        if(f)cout << 1;
+        else cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
