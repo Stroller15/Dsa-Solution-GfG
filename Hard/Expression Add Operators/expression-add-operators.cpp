c705214a8943e0a85729c6e38cd23e4559afaffb
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-05-25 23:51:08 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Expression Add Operators/expression-add-operators.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+     void solve(int ind,string s,int target,vector<string> &res,string tmp,long long prev,long long sum){
+      //Base case when the index becomes equal to the size of the given string
+      if(ind==s.size()){
+          //iff sum==target then only we push the temporary path to the res
+          if(sum==target)
+            res.push_back(tmp);
+          return;
+      }
+      //this is used for the case "1+23" for that we should require another string for the for loop
+      string st="";
+      //this is used to get the value of the numbers taken from the string
+      long long curr=0;
+      for(int i=ind;i<s.size();i++){
+          //this is the case for any leading 0s in which we have to do nothing just break
+          if(i>ind && s[ind]=='0')
+            break;
+          //preparation of the strings that are taken in each iteration
+          st+=s[i];
+          //conversion of prepared string to number for further computation,
+          //we are using stoll since the value can be really greater
+          curr=stoll(st);
+          
+          //if its first index to be taken, then there is only one case that it is to be taken
+          if(ind==0)
+          solve(i+1,s,target,res,tmp+st,curr,curr);
+          
+          //apart from the first index, there are three cases
+          else{
+              solve(i+1,s,target,res,tmp+"+"+st,curr,sum+curr);
+              solve(i+1,s,target,res,tmp+"-"+st,-curr,sum-curr);
+              
+              //bodmass rule comes in picture while doing multiplication, for this only we were storing the previous value
+              solve(i+1,s,target,res,tmp+"*"+st,prev*curr,sum-prev+prev*curr);
+          }
+      }
+      return;
+  }
+    vector<string> addOperators(string s, int target) {
+        //to store the result that is vector of strings
+        vector<string> res;
+        
+        //to store the temporary path that will be created in the recursive calls
+        string tmp="";
+        
+        //this is used to handle the "*" case since in bodmass first the multiplication is done before "+" or "-"
+        long long prev=0;
+        
+        solve(0,s,target,res,tmp,prev,0);
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int target;
+        string S;
+        cin >> S >> target;
+
+        Solution obj;
+        vector<string> ans = obj.addOperators(S, target);
+        sort(ans.begin(), ans.end());
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
