b21af076902830510783fbb96fe41154f07c5d1f
Shubham Verma <84174011+Stroller15@users.noreply.github.com>
2023-10-20 14:29:34 +0530

    Updated Solution - GfG to GitHub

--- Easy/Special Stack/special-stack.cpp
+++ Easy/Special Stack/special-stack.cpp
@@ -27,53 +27,119 @@ int main(){
 }
 // } Driver Code Ends
 
-stack<int> ss;
+
+// With extra space taking a supportive stack
+
+// stack<int> ss;
+
+// void push(stack<int>& s, int a){
+// 	// Your code goes here
+// 	s.push(a);
+	
+// 	//check for suportive stack
+// 	if(ss.size() == 0 || a <= ss.top()) {
+// 	    ss.push(a);
+// 	}
+// }
+
+// bool isFull(stack<int>& s,int n){
+// 	// Your code goes here
+// 	if(s.size() == n) {
+// 	    return true;
+// 	}
+// 	else {
+// 	    return false;
+// 	}
+// }
+
+// bool isEmpty(stack<int>& s){
+// 	// Your code goes here
+//     return s.empty();
+// }
+
+// int pop(stack<int>& s){
+// 	// Your code goes here
+// 	int poppedElement;
+	
+// 	if(!s.empty()) {
+// 	    poppedElement = s.top();
+// 	    s.pop();
+// 	}
+	
+	
+// 	//check on suportive stack
+// 	if(ss.top() == poppedElement) {
+// 	    ss.pop();
+// 	}
+	
+// 	return poppedElement;
+// }
+
+// int getMin(stack<int>& s){
+// 	// Your code goes here
+// 	return ss.top();
+// }
+
+//---------------------------------------------------------------------------
+
+int minElement;
 
 void push(stack<int>& s, int a){
 	// Your code goes here
-	s.push(a);
-	
-	//check for suportive stack
-	if(ss.size() == 0 || a <= ss.top()) {
-	    ss.push(a);
+	if(s.empty()) {
+	    s.push(a);
+	    minElement = a;
+	} else {
+	    if(a > minElement) {
+	        s.push(a);
+	    } else if(a <= minElement) {
+	        s.push(2*a - minElement);
+	        minElement = a;
+	    }
 	}
+	
+	
 }
 
 bool isFull(stack<int>& s,int n){
 	// Your code goes here
-	if(s.size() == n) {
-	    return true;
-	}
-	else {
-	    return false;
-	}
+	return (s.size() == n);
+
 }
 
 bool isEmpty(stack<int>& s){
 	// Your code goes here
-    return s.empty();
+	return s.empty();
+    
 }
 
 int pop(stack<int>& s){
 	// Your code goes here
-	int poppedElement;
-	
-	if(!s.empty()) {
-	    poppedElement = s.top();
-	    s.pop();
+	if(s.empty()) {
+	    return -1;
+	} else {
+	    if(s.top() > minElement) {
+	        int poppedElement = s.top();
+	        s.pop();
+	    } else if(s.top() <= minElement) {
+	        minElement = 2 * minElement - 1;
+	        s.pop();
+	        return minElement;
+	    }
 	}
-	
-	
-	//check on suportive stack
-	if(ss.top() == poppedElement) {
-	    ss.pop();
-	}
-	
-	return poppedElement;
+
 }
 
 int getMin(stack<int>& s){
 	// Your code goes here
-	return ss.top();
+	if(s.empty()) {
+	    return -1;
+	}else {
+	    return minElement;
+	}
 }
+
+
+
+
 
